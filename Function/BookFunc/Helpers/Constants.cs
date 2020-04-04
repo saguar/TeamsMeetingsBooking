585583ec75a948a0b550b1ceacd38df35bcf26ad
Fulvio Mercoliano <fumer@microsoft.com>
2020-04-04 14:53:53 +0200

    Add comment on ConfigConstants

--- Function/BookFunc/Helpers/Constants.cs
+++ Function/BookFunc/Helpers/Constants.cs
@@ -6,6 +6,7 @@ namespace TeamsMeetingBookFunc.Helpers
 {
     internal class ConfigConstants
     {
+        //If you change those values, please update the ARM template
         internal const string TenantIdCfg = "TenantID";
         internal const string ClientIdCfg = "ClientID";
         internal const string UserPasswordCfg = "UserPassword";
